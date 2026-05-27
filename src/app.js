6020779bdc1231f7b932944a591b64d4e2476ee5
MarlaSinger2025 <Marla_Singer_2020@web.de>
2026-05-27 13:15:20 +0200

    added more markers and adjusted map zoom

--- src/app.js
+++ src/app.js
@@ -17,3 +17,36 @@ Leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://google.com" target="_blank">Google</a>',
 }).addTo(map);
 
+const myLocations = [
+   {
+    name: 'WBS CODING SCHOOL',
+    location: [52.457131, 13.54007],
+    description: 'The best coding school in the world'
+  },
+  {
+    name: 'Alexanderplatz',
+    location: [52.521918, 13.413215],
+    description: 'The most famous square in Berlin'
+  },
+  {
+    name: 'Brandenburg Gate',
+    location: [52.516275, 13.377704],
+    description: 'The most famous gate in Berlin'
+  },
+  {
+    name: 'Berlin Wall',
+    location: [52.507541, 13.39032],
+    description: 'The most famous wall in Berlin'
+  }
+];
+
+//Add markers to the map with a popup with a forEach loop
+myLocations.forEach(location => {
+    Leaflet.marker(location.location, { icon: markerIcon})
+    .bindPopup(location.description)
+    .addTo(map);
+});
+
+// Set the view of the map so all markers are in view at once
+const bounds = Leaflet.latLngBounds(myLocations.map(location => location.location));
+map.fitBounds(bounds);
